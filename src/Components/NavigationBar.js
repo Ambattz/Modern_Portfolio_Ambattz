72d28c878412182912839744e337c76f882b4540
[Ambattz} <[alvingeorge30@gmail.com]>
2022-09-06 02:07:58 +0530

    add NavBar - reformatted

--- src/Components/NavigationBar.js
+++ src/Components/NavigationBar.js
@@ -1,52 +1,46 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import { Box, Grid } from '@mui/material';
+//Material UI Imports
+import { Grid, AppBar, Box, Container } from '@mui/material';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
+import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
-import MotionPhotosAutoIcon from '@mui/icons-material/MotionPhotosAuto'; import { amber } from '@mui/material/colors';
-import Fade from '@mui/material/Fade';
-import AvatharImg from '../StaticFiles/avathar.jpg';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
+import { amber } from '@mui/material/colors';
+import Fade from '@mui/material/Fade';
+import MotionPhotosAutoIcon from '@mui/icons-material/MotionPhotosAuto';
+//Custom Imports
+import AvatharImg from '../StaticFiles/avathar.jpg';
 
 const theme = createTheme({
     palette: {
         primary: {
             main: amber[500],
             contrastText: '#064663'
-        }
-    },
+        },
+        background: {
+            paper: amber[500]
+        },
+    }
 });
-
 
 const pages = ['Home', 'Profile', 'Expertise', 'Contact'];
 
 const NavigationBar = () => {
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
     };
 
     return (
@@ -77,7 +71,7 @@ const NavigationBar = () => {
                             >
                                 mbattz
                             </Typography>
-                            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                            <Box sx={{ color: 'primary', flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                                 <IconButton
                                     size="large"
                                     aria-label="account of current user"
@@ -86,13 +80,13 @@ const NavigationBar = () => {
                                     onClick={handleOpenNavMenu}
                                     color="inherit"
                                 >
-                                    <MenuIcon />
+                                    {Boolean(anchorElNav) ? <MenuOpenIcon /> : <MenuIcon />}
                                 </IconButton>
                                 <Menu
                                     id="menu-appbar"
                                     anchorEl={anchorElNav}
                                     anchorOrigin={{
-                                        vertical: 'bottom',
+                                        vertical: 60,
                                         horizontal: 'left',
                                     }}
                                     keepMounted
@@ -107,20 +101,21 @@ const NavigationBar = () => {
                                     }}
                                 >
                                     {pages.map((page) => (
-                                        <MenuItem key={page} onClick={handleCloseNavMenu}>
-                                            <Typography textAlign="center">{page}</Typography>
+                                        <MenuItem key={page} onClick={handleCloseNavMenu} >
+                                            <Typography textAlign="center" sx={{ color: 'inherit', }}>
+                                                {page}
+                                            </Typography>
                                         </MenuItem>
                                     ))}
                                 </Menu>
                             </Box>
-                            <MotionPhotosAutoIcon titleAccess="Alvin Logo" fontSize='large' sx={{ display: { xs: 'flex', md: 'none' }, mr: 0.5 }} />
+                            <MotionPhotosAutoIcon titleAccess="Alvin Logo" fontSize='large' sx={{ display: { xs: 'flex', md: 'none' } }} />
                             <Typography
                                 variant="h5"
                                 noWrap
                                 component="a"
                                 href=""
                                 sx={{
-                                    mr: 3,
                                     display: { xs: 'flex', md: 'none' },
                                     flexGrow: 1,
                                     fontFamily: 'cursive',
@@ -137,14 +132,13 @@ const NavigationBar = () => {
                                     <Button
                                         key={page}
                                         onClick={handleCloseNavMenu}
-                                        sx={{ my: 2, color: 'white', display: 'block' }}
+                                        sx={{ my: 2, color: 'inherit', display: 'block' }}
                                     >
                                         {page}
                                     </Button>
                                 ))}
                             </Box>
-
-                            <Box sx={{ flexGrow: 0, ml: 3 }}>
+                            <Box sx={{ flexGrow: 0, ml: 2 }}>
                                 <Tooltip
                                     title="Alvin Ambattu"
                                     arrow

9129cb170f742e1f7f884bf6a2ad0e55aecb17b7
[Ambattz} <[alvingeorge30@gmail.com]>
2022-09-07 02:18:05 +0530

    edit Home "size & color"

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,19 +1,16 @@
 import * as React from 'react';
-//Material UI Imports
-import { Grid, AppBar, Box, Container } from '@mui/material';
-import Toolbar from '@mui/material/Toolbar';
+// Material UI Imports
+import { Grid, Box, Container } from '@mui/material';
+import { AppBar, Toolbar } from '@mui/material';
+import { Menu, MenuItem } from '@mui/material';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
-import { MotionPhotosAuto, HomeRounded, Person4Rounded, TerminalRounded, ContactMailRounded } from '@mui/icons-material/';
-
-//Custom Imports
+import MenuIcon from '@mui/icons-material/Menu'
+import { MenuOpen, MotionPhotosAuto, HomeRounded, Person4Rounded, TerminalRounded, ContactMailRounded } from '@mui/icons-material/';
+// Custom Imports
 import { Avathar, MenuButton } from '../helper/directoryHelper';
 
 const pagesInfo = { 'Home': <HomeRounded />, 'Profile': <Person4Rounded />, 'Expertise': < TerminalRounded />, 'Contact': < ContactMailRounded /> };
@@ -30,7 +27,7 @@ const NavigationBar = () => {
     };
 
     return (
-        <Grid container alignContent="center" justifyContent="center">
+        <Grid container justifyContent="center">
             <AppBar sx={{
                 width: 'fit-content',
                 borderRadius: 5,
@@ -40,7 +37,11 @@ const NavigationBar = () => {
             }}>
                 <Container maxWidth="lg">
                     <Toolbar disableGutters>
-                        <MotionPhotosAuto titleAccess="Alvin Logo" fontSize='large' sx={{ display: { xs: 'none', md: 'flex' }, mr: 0.5 }} />
+                        <MotionPhotosAuto
+                            titleAccess="Alvin Logo"
+                            fontSize='large'
+                            sx={{ display: { xs: 'none', md: 'flex' }, mr: 0.5, color: "#041c32" }}
+                        />
                         <Typography
                             variant="h6"
                             noWrap
@@ -52,18 +53,18 @@ const NavigationBar = () => {
                                 fontFamily: 'cursive',
                                 fontWeight: 800,
                                 letterSpacing: '.2rem',
-                                color: 'inherit',
+                                color: "#041c32",
                                 textDecoration: 'none',
                             }}
                         >
                             mbattz
                         </Typography>
-                        <Box sx={{ color: 'primary', flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <Box sx={{ color: "#041c32", flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                             <MenuButton
                                 state={Boolean(anchorElNav)}
                                 onClick={handleOpenNavMenu}
                             >
-                                {Boolean(anchorElNav) ? <MenuOpenIcon /> : <MenuIcon />}
+                                {Boolean(anchorElNav) ? <MenuOpen /> : <MenuIcon />}
                             </MenuButton>
                             <Menu
                                 id="menu-appbar"
@@ -98,14 +99,19 @@ const NavigationBar = () => {
                                         }
                                     }}>
                                         {pageIcon}
-                                        <Typography textAlign="center" sx={{ ml: 1, color: 'inherit', }}>
+                                        <Typography textAlign="center" sx={{ ml: 1, color: "#041c32", }}>
                                             {pageName}
                                         </Typography>
                                     </MenuItem>
                                 ))}
                             </Menu>
                         </Box>
-                        <MotionPhotosAuto titleAccess="Alvin Logo" fontSize='large' sx={{ display: { xs: 'flex', md: 'none' } }} />
+                        <MotionPhotosAuto
+                            titleAccess="Alvin Logo"
+                            fontSize='large'
+                            sx={{ display: { xs: 'flex', md: 'none' }, color: "#041c32" }}
+
+                        />
                         <Typography
                             variant="h5"
                             noWrap
@@ -117,28 +123,31 @@ const NavigationBar = () => {
                                 fontFamily: 'cursive',
                                 fontWeight: 800,
                                 letterSpacing: '.2rem',
-                                color: 'inherit',
+                                color: "#041c32",
                                 textDecoration: 'none',
                             }}
                         >
                             mbattz
                         </Typography>
-                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' }, color: "#041c32" }}>
                             {Object.entries(pagesInfo).map(([pageName, pageIcon]) => (
                                 <Button
                                     key={pageName}
+                                    startIcon={pageIcon}
                                     onClick={handleCloseNavMenu}
+                                    size="medium"
                                     sx={{
-                                        my: 2, color: 'inherit', display: 'block',
+                                        my: 2, color: "#041c32", display: 'block',
                                         "&:hover": {
-                                            color: "primary.main",
-                                            backgroundColor: "primary.contrastText",
+                                            color: "#ffc107",
+                                            backgroundColor: "#041c32",
                                             cursor: "default",
                                         },
                                     }}
                                 >
-                                    {pageIcon}
+                                    {pageName}
                                 </Button>
+
                             ))}
                         </Box>
                         <Box sx={{ flexGrow: 0, ml: 2 }}>
@@ -150,7 +159,7 @@ const NavigationBar = () => {
                                 <Avatar
                                     alt="Alvin Ambattu"
                                     src={Avathar}
-                                    sx={{ width: 45, height: 45 }}
+                                    sx={{ width: 50, height: 50 }}
                                     variant="rounded"
                                 />
                             </Tooltip>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,60 +1,123 @@
 import * as React from 'react';
+// Material UI Imports
 import { Box, Typography } from '@mui/material';
+//Third-Party Imports
 import Typewriter from 'typewriter-effect';
+
 function HomePage() {
     return (
-        <Box sx={{
-            display: { xs: 'none', md: 'flex' },
-            margin: 0,
-            padding: 0,
-            justifyContent: 'center',
-            alignItems: 'center',
-            flexDirection: 'column',
-            width: 'auto',
-            height: '100vh'
-        }}
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center" >
-            <Box
+        <React.Fragment>
+            <Box sx={{
+                display: { xs: 'flex', md: 'none' },
+                width: 'auto',
+                height: '100vh'
+            }}
                 display="flex"
-                flexDirection="row"
+                flexDirection="column"
                 alignItems="center"
-                justifyContent="center">
-                <Typography
-                    sx={{
-                        fontSize: 80,
-                        color: 'text.secondary',
-                        pr: 3
-                    }}>I'm </Typography>
-                <Typography
-                    sx={{
-                        fontSize: 80,
-                        color: 'text.secondary',
-                    }}>
-                    <Typewriter
-                        options={{
-                            strings: ["Alvin Ambattu", "an Engineer", "a Web Developer"],
-                            autoStart: true,
-                            loop: true,
-                        }}
-                    />
-                </Typography>
-            </Box>
-            <Box
-                display="flex"
-                flexDirection="row"
-                alignItems="center"
-                justifyContent="center">
-                <Typography
-                    sx={{
-                        fontSize: 30,
-                        color: 'text.secondary',
+                justifyContent="center" >
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="center">
+                    <Typography
+                        sx={{
+                            fontSize: 40,
+                            color: '#4e9f3d',
+                            pr: 1
+                        }}>I'm</Typography>
+                    <Typography
+                        sx={{
+                            fontSize: 40,
+                            color: 'text.secondary',
+                        }}>
+                        <Typewriter
+                            options={{
+                                strings: ["Alvin Ambattu", "an Engineer", "a Web Developer"],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </Typography>
+                </Box>
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="center">
+                    <Typography
+                        sx={{
+                            fontSize: 15,
+                            color: '#4e9f3d',
 
-                    }}>A high-yielding IT GEEK </Typography>
-            </Box>
-        </Box >
+                        }}>A high-yielding &nbsp;
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: 20,
+                            color: 'text.secondary',
+
+                        }}>IT GEEK
+                    </Typography>
+                </Box>
+            </Box >
+            <Box sx={{
+                display: { xs: 'none', md: 'flex' },
+                width: 'auto',
+                height: '100vh'
+            }}
+                display="flex"
+                flexDirection="column"
+                alignItems="center"
+                justifyContent="center" >
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="center">
+                    <Typography
+                        sx={{
+                            fontSize: 100,
+                            color: '#4e9f3d',
+                            pr: 3
+                        }}>I'm </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: 100,
+                            color: 'text.secondary',
+                        }}>
+                        <Typewriter
+                            options={{
+                                strings: ["Alvin Ambattu", "an Engineer", "a Web Developer"],
+                                autoStart: true,
+                                loop: true,
+                            }}
+                        />
+                    </Typography>
+                </Box>
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    alignItems="center"
+                    justifyContent="center">
+                    <Typography
+                        sx={{
+                            fontSize: 20,
+                            color: '#4e9f3d',
+
+                        }}>A high-yielding &nbsp;
+                    </Typography>
+                    <Typography
+                        sx={{
+                            fontSize: 30,
+                            color: 'text.secondary',
+
+                        }}>IT GEEK
+                    </Typography>
+                </Box>
+            </Box >
+        </React.Fragment>
     )
 }
 
